816985b4e22428aae6e7c69de54c284bb8249eb8
kyanagis <kyanagis42@gmail.com>
2026-07-21 14:22:56 +0900

    fic

--- cxx-Module/08/ex00/main.cpp
+++ cxx-Module/08/ex00/main.cpp
@@ -1,5 +1,6 @@
 #include "easyfind.hpp"
 
+#include <cstddef>
 #include <iostream>
 #include <list>
 #include <vector>
@@ -24,6 +25,48 @@ static void search_list(const std::list<int>& numbers, int value) {
 	}
 }
 
+static void print_vector_addresses(const std::vector<int>& numbers) {
+	for (std::size_t i = 0; i < numbers.size(); ++i) {
+		std::cout << "  " << numbers[i] << " @ " << &numbers[i] << '\n';
+	}
+}
+
+static void print_list_addresses(const std::list<int>& numbers) {
+	std::list<int>::const_iterator it;
+	for (it = numbers.begin(); it != numbers.end(); ++it) {
+		std::cout << "  " << *it << " @ " << &*it << '\n';
+	}
+}
+
+static void reuse_freed_node(std::list<int>& numbers) {
+	std::list<int>::iterator it;
+
+	numbers.push_front(-1);
+	it = numbers.begin();
+	++it;
+	++it;
+	numbers.erase(it);
+	numbers.push_back(99);
+}
+
+static void show_memory_layout() {
+	std::vector<int> contiguous;
+	std::list<int> nodes;
+
+	for (int i = 0; i < 5; ++i) {
+		contiguous.push_back(i);
+		nodes.push_back(i);
+	}
+	std::cout << "--- vector: address == base + index * sizeof(int) ---\n";
+	print_vector_addresses(contiguous);
+	std::cout << "--- list: independently allocated nodes ---\n";
+	print_list_addresses(nodes);
+	reuse_freed_node(nodes);
+	std::cout << "--- list: after erase and push_back ---\n";
+	print_list_addresses(nodes);
+	std::cout << "traversal order no longer follows address order\n";
+}
+
 int main() {
 	std::vector<int> numbers;
 	for (int i = 0; i < 10; ++i) {
@@ -45,6 +88,8 @@ int main() {
 
 	std::vector<int> empty;
 	search_vector(empty, 1);
+
+	show_memory_layout();
 
 	return 0;
 }
